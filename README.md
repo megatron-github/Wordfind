d9e52fbc58989f674290228c397f1876ca1259ea
megatron-github <tpham@hamilton.edu>
2023-11-05 02:01:39 +0700

    Updated wordfind.py and README.md
    [MOD]
    - Rewrite the functions in file wordfind.py
    - Add a sample run to README.md

--- README.md
+++ README.md
@@ -1,3 +1,54 @@
 # Wordfind
 
-Wordfind is a project that shows how a Wordsearch puzzle can be solved using a simple programming algorithm. The program begins with two intended parameters: a grid of lower-case single-lettered strings and a list of lower-case words. The goal is to find all the given words from the grid, if possible. A word can occur in any of the eight directions. Multiple words can overlap in one or more letters. However, each word can only found once in the puzzle. When the program found the word, it will capitalize each letter comprising the word in the grid. After searching through the entire grid, the program will return the number of founded words. 
+## Background
+
+Given a nested list of lowercased letters to represent the word grid and a list of lowercased words, we want to write a program to find all the lowercased words in the word grid, if possible.
+
+## Approach
+
+For each word we find, we will capitlize the letters of that word on the grid. After searching through the entire grid, the program will print the grid and the number of founded words.
+
+A word may lies vertically, horizontally, or diagonally in the word grid. The order of the letters can be forward or backward. Multiple words may overlap one or more letters. Each word may only found once in the grid and the letters must line up in only one direction.
+
+Given the conditions, we may break down the entire program into small helper functions such as:
+
+    1. A function to get the word from the word list
+    2. A function to find the starting coordinates of each word
+    3. A function to feed the checking directions
+    4. Following the each starting coordinates, a function to check whether all the letters of a given word lined up in the given direction
+    5. A function to check if current checking position is within the grid
+
+## Sample run
+Given a list of words:
+```
+['meow', 'wed', 'do', 'justice']
+```
+and a grid of letters:
+```
+j m w e
+e e p p
+q o x u
+w w e d
+w g j o
+```
+The program will print:
+```
+j M w e
+e E p p
+q O x u
+w W E D
+w g j O
+Found 3 words
+```
+
+## How to run
+
+To run the program and play Yahtzee
+```
+python3 wordfind.py
+```
+
+## Author
+
+Truong Pham
+
--- wordfind.py
+++ wordfind.py
@@ -1,155 +1,109 @@
+#!/usr/bin/env python3
 """
- *****************************************************************************
-   FILE:        wordfind.py
+****************************************************************************************************
+    FILE:        wordfind.py
 
-   DESCRIPTION: With a given grid of letters, and a list of words. Find all
-                the words in the grid, if possible. Report how many word in
-                are actually found in the grid. Print all found words in the
-                grid in uppercase.
+    DESCRIPTION: With a given grid of letters, and a list of words. Find all the words in the grid,
+        if possible. Report the number of word found in the grid. Capitalize the letters of the
+        found words in the grid.
 
- *****************************************************************************
+ ***************************************************************************************************
 """
-
 
 def printGrid(grid):
-    """ Display the grid in a nice way """
+    """ Display the grid """
 
     for row in grid:
-        print(row)
+        outputStr = ""
+        for letter in row:
+            outputStr += letter + " "
+        print(outputStr)
 
-def confirm_pattern(grid, letters_list, letter_row, letter_col,
-                    dr_row, dr_col):
-    """ From each location of first letter (starting position), find the
-        neigbor letter in given direction (if in a given direction, the
-        neigbor is out of bounds, immediately quit). for each neigbor letter
-        in the given direction, check if that neigbor letter is the second
-        letter in word. If yes, continue and compare third neigbor and so on.
-        If not, change direction. """
+def confirm_pattern(grid, letters, letter_row, letter_col, dir_row, dir_col):
+    """ Given a starting coordinate and a checking direction, check if all the letters all the
+        letters of the given word lined up. """
 
     patterns = []
-
-    # For each location that is followed the starting location (the location
-    # of the first letter in each word), check if that location is still in
-    # grid. If yes, then check if the letter in that location is the letter
-    # that followed the first letter in word. If yes, check the third letter,
-    # and so on. If one of the condition above is wrong, change direction.
-    for letter in letters_list:
-        if is_in_bounds(grid, letter_row, letter_col) is True:
-            if letter is grid[letter_row][letter_col]:
-                patterns.append((letter, (letter_row, letter_col)))
-        letter_row += dr_row
-        letter_col += dr_col
+    for letter in letters:
+        if not is_in_bounds(grid, letter_row, letter_col) or \
+          letter != grid[letter_row][letter_col]:
+            return None
+        patterns.append((letter_row, letter_col))
+        letter_row += dir_row
+        letter_col += dir_col
     return patterns
 
-def find_pattern_in(grid, letters_list, start_row, start_col):
-    """ For each direction from the first letter (starting position),
-        examine if there is a pattern. A pattern exists when all letters
-        of a word are located in one direction. """
+def find_pattern_with(grid, letters, start_row, start_col):
+    """ Given all the directions, check if there exists a direction in which all the letters
+        of the given word lined up. """
 
     directions = [(-1, 0), (-1, 1), (0, 1), (1, 1),
                   (1, 0), (1, -1), (0, -1), (-1, -1)]
 
-    # For each direction, if the letters that are followed the first letter
-    # are the letters in word. If not, change direction.
-    for dr in directions:
-        dr_row = dr[0]
-        dr_col = dr[1]
-        result = confirm_pattern(grid, letters_list,
-                                 start_row, start_col,
-                                 dr_row, dr_col)
-        if len(letters_list) == len(result):
+    for dir_row, dir_col in directions:
+        result = confirm_pattern(grid, letters, start_row, start_col, dir_row, dir_col)
+        if result:
             return result
     return None
 
-def find_pattern(grid, letters_list, start_location):
-    """ For each location of the first letter of the word, try to find
-        if one of the directions continues to spelling out the word. """
+def find_pattern(grid, letters, starting_coords):
+    """ From each coordinate of the starting letter, check if the given word started
+        at that coordinate. """
 
-    # From each starting location (first letter of a word), look at all
-    # directions and find a pattern (a pattern is found when all letters
-    # in a word is found in a single direction)
-    for loc in start_location:
-        start_row = loc[0]
-        start_col = loc[1]
-        patterns = find_pattern_in(grid, letters_list,
-                                   start_row, start_col)
-        if patterns is not None:
-            return patterns
+    for start_row, start_col in starting_coords:
+        found_pattern = find_pattern_with(grid, letters, start_row, start_col)
+        if found_pattern:
+            return found_pattern
     return None
 
-def is_in_bounds(grid, possible_row, possible_col):
+def is_in_bounds(grid, row, col):
     """ Return True if loc_tuple is a legal position within grid.
         Return False otherwise. """
 
-    # If (row_loc, col_loc) are smaller than zero or bigger than grid_size, 
-    # then (row_loc, col_loc) is outside of grid. Man states len(grid[0]) 
-    # will find the number of column of the first row, which is also the 
-    # number of column of the whole grid.
-    if possible_row < 0 or possible_row >= len(grid):
+    if row < 0 or row >= len(grid):
         return False
-    if possible_col < 0 or possible_col >= len(grid[0]):
+    if col < 0 or col >= len(grid[0]):
         return False
     return True
 
+def locate_first_letter(grid, starting_letter):
+    """ Return a list of the coordinates (tuples) of the starting letter in the grid. """
 
-def locate_first_letter(grid, letter_list):
-    """ Return a list of the locations (tuples) of target within grid. """
-
-    # For each word, use the first letter in the list of letters, and return
-    # all locations (in the form of list) that the letter of the word
-    # is found in grid.
-    letter_location = []
-    for letter in letter_list:
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] is letter:
-                    letter_location.append((i, j))
-    return letter_location
+    starting_coords = []
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == starting_letter:
+                starting_coords.append((i, j))
+    return starting_coords
 
 def dissect_word(word):
     """ Dissect each word into letters and put them into a list"""
 
-    # Make a new list that will break down each into letters, return
-    # the list of letters back to wordfind function.
-    letters_list = []
-    for letter in word:
-        letters_list.append(letter)
-    return letters_list
+    return [letter for letter in word]
 
 def wordfind(grid, words):
-    """ For each word in words, if possible, find it once in the grid, case
-        insensitive.  Convert those found letters in the grid
-        to upper-case."""
+    """ For each word in words, find it once in the grid, if possible.
+        Capitalize the letters of the found word in the grid."""
 
-    pattern_loc = []
+    word_locs = []
     total_word = 0
     for word in words:
-
         # Dissect each word into letters.
-        letters_list = dissect_word(word)
-
-        # Find all locations of the first letter.
-        start_location = locate_first_letter(grid, letters_list[0])
-
+        letters = dissect_word(word)
+        # Find the coordinates of the first letter.
+        starting_coords = locate_first_letter(grid, letters[0])
         # From all the starting location (first letter of the word), look
-        # in all directions until found the next letters in the word at the
-        # in the same direction.
-        patterns = find_pattern(grid, letters_list, start_location)
-
-        # If there is a pattern found from a starting location, a word is
-        # found. Print that pattern in uppercase in grid.
-        if patterns is not None:
+        # in all directions to check whether all the letters of the word
+        # lined up in the same direction.
+        found_pattern = find_pattern(grid, letters, starting_coords)
+        # If in a direction, all the letters of the word lined up, then
+        # a word is found in the grid. Save the coordinates of the letters
+        if found_pattern:
             total_word += 1
-            for tple in patterns:
-                row_loc = tple[1][0]
-                col_loc = tple[1][1]
-                pattern_loc.append((row_loc, col_loc))
+            word_locs += found_pattern
 
-    # change the original grid to a grid that
-    # had the need words in uppercase.
-    for tple in pattern_loc:
-        row_loc = tple[0]
-        col_loc = tple[1]
+    # Change the original grid to a grid that had the found words in uppercase.
+    for row_loc, col_loc in word_locs:
         grid[row_loc][col_loc] = grid[row_loc][col_loc].upper()
 
     return total_word
@@ -167,7 +121,5 @@ def main():
     printGrid(myGrid)
     print("Found {} words".format(count))
 
-# this invokes the main function.  It is always included in our
-# python programs
 if __name__ == "__main__":
     main()
